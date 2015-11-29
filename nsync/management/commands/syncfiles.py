e95e7153c8187309826ec22fb501caeeec668322
Andrew Dodd <andrew.john.dodd@gmail.com>
2015-11-29 21:34:57 +0100

    Including multi-file command
    This is the real meat of the system. Not completely unit tested, but the
    integration tests are all working!

---
+++ nsync/management/commands/syncfiles.py
@@ -0,0 +1,75 @@
+from django.core.management.base import BaseCommand, CommandError
+from django.apps.registry import apps
+from django.db import transaction
+import os
+import csv
+import argparse
+import re
+
+from nsync.models import ExternalSystem
+from nsync.sync import ExternalSystemHelper, ModelFinder, SupportedFileChecker
+from nsync.actions import CsvActionsBuilder
+from nsync.policies import OrderedSyncPolicy, TransactionSyncPolicy
+
+
+DEFAULT_FILE_REGEX=r'(?P<external_system>[a-zA-Z0-9]+)_(?P<app_name>[a-zA-Z0-9]+)_(?P<model_name>[a-zA-Z0-9]+).*\.csv'
+
+
+class Command(BaseCommand):
+    help = 'Sync info from a list of files'
+
+    def add_arguments(self, parser):
+        # Mandatory
+        parser.add_argument('files', type=argparse.FileType('r'), nargs='+')
+        # Optional
+        parser.add_argument('--file_name_regex',
+                type=str,
+                default=DEFAULT_FILE_REGEX,
+                help='The regular expression to obtain the system name, app name and model name from each file')
+        parser.add_argument('--create_external_system',
+                type=bool,
+                default=True,
+                help='If true, the command will create a matching external system object if one cannot be found')
+
+    def handle(self, *args, **options):
+        TestableCommand(**options)()
+
+class TestableCommand:
+    def __init__(self, **options):
+        self.files = options['files']
+        self.pattern = re.compile(options['file_name_regex'])
+        self.create_external_system = options['create_external_system']
+
+    def __call__(self):
+        pass
+
+        actions = []
+
+        for f in self.files:
+            if not SupportedFileChecker.is_valid(f):
+                raise CommandError('Unsupported file:{}'.format(filename))
+
+            basename = os.path.basename(f.name)
+            (system, app, model) = TargetExtractor(self.pattern).extract(basename)
+            
+            external_system = ExternalSystemHelper.find(system, self.create_external_system)
+            model = ModelFinder.find(app, model)
+
+            reader = csv.DictReader(f)
+            builder = CsvActionsBuilder(model, external_system)
+            for d in reader:
+                actions.extend(builder.from_dict(d))
+
+
+        TransactionSyncPolicy(OrderedSyncPolicy(actions)).execute()
+
+
+class TargetExtractor:
+    def __init__(self, pattern):
+        self.pattern = pattern
+
+    def extract(self, filename):
+        result = self.pattern.match(filename)
+        return (result.group('external_system'),
+                result.group('app_name'),
+                result.group('model_name'))
---
+++ nsync/management/commands/test_syncfiles.py
@@ -0,0 +1,214 @@
+from django.core.management import call_command
+from django.core.exceptions import MultipleObjectsReturned, ObjectDoesNotExist, FieldError
+from django.contrib.contenttypes.models import ContentType
+from django.test import TestCase
+from unittest.mock import MagicMock, patch, ANY
+
+from .syncfiles import Command, TestableCommand, TargetExtractor, DEFAULT_FILE_REGEX
+from django.core.management.base import CommandError
+
+from nsync.models import ExternalKeyMapping, ExternalSystem
+import tempfile
+import re
+from nsync.tests.models import TestHouse
+import io
+
+class TestSyncFilesCommand(TestCase):
+    def test_command_raises_error_if_file_list_is_empty(self):
+        with self.assertRaises(CommandError):
+            call_command('syncfiles')
+
+    def test_command_raises_error_if_regex_does_not_compile(self):
+        f = tempfile.NamedTemporaryFile()
+        import sre_constants
+        with self.assertRaises(sre_constants.error):
+            call_command('syncfiles', f.name, file_name_regex='(((')
+
+    def xtest_test(self):
+        f1 = tempfile.NamedTemporaryFile(mode='w', prefix='Temp_tests_TestHouse_', suffix='.csv')
+        f2 = tempfile.NamedTemporaryFile()
+        f3 = tempfile.NamedTemporaryFile()
+
+        call_command('syncfiles', f1.name, f2.name, f3.name)
+
+class TestTestableCommand(TestCase):
+    @patch('nsync.management.commands.syncfile.CsvActionsBuilder')
+    @patch('csv.DictReader')
+    def xtest_data_flow(self, DictReader, ActionsBuilder):
+        file = MagicMock()
+        row = MagicMock()
+        row_provider = MagicMock()
+        DictReader.return_value = row_provider
+        row_provider.__iter__.return_value = [row]
+        model_mock = MagicMock()
+        external_system_mock = MagicMock()
+        action_mock = MagicMock()
+        ActionsBuilder.return_value.from_dict.return_value = [action_mock]
+        TestableCommand.sync(external_system_mock, model_mock, file)
+        DictReader.assert_called_with(file)
+        ActionsBuilder.assert_called_with(model_mock, external_system_mock)
+
+        ActionsBuilder.return_value.from_dict.assert_called_with(row)
+        action_mock.execute.assert_called_once_with()
+
+class TestTargetExtractor(TestCase):
+    def setUp(self):
+        self.sut = TargetExtractor(re.compile(DEFAULT_FILE_REGEX))
+
+    def test_it_extracts_the_correct_strings_from_the_filename(self):
+        self.assertEquals(('System', 'App', 'Model'), self.sut.extract('System_App_Model.csv'))
+        self.assertEquals(('System', 'App', 'Model'), self.sut.extract('System_App_Model_1234.csv'))
+        self.assertEquals(('ABCabc123', 'App', 'Model'), self.sut.extract('ABCabc123_App_Model.csv'))
+        
+class TestSyncSingleFileIntegrationTests(TestCase):
+    def test_create_and_update(self):
+        house1 = TestHouse.objects.create(address='House1')
+        house2 = TestHouse.objects.create(address='House2')
+        house3 = TestHouse.objects.create(address='House3', country='Belgium')
+        house4 = TestHouse.objects.create(address='House4', country='Belgium')
+
+        csv_file_obj = tempfile.NamedTemporaryFile(mode='w', prefix='TestSystem_tests_TestHouse_', suffix='.csv')
+        csv_file_obj.writelines([
+            'action_flags,match_field_name,address,country\n',
+            'c,address,House1,Australia\n', # Should have no effect
+            'u,address,House2,Australia\n', # Should update country
+            'u,address,House3,Australia\n', # Should have no effect
+            'u*,address,House4,Australia\n', # Should update country
+            'c,address,House5,Australia\n', # Should create new house
+            ])
+        csv_file_obj.seek(0)
+
+        call_command('syncfiles', csv_file_obj.name)
+
+        for house in [house1, house2, house3, house4]:
+            house.refresh_from_db()
+
+        self.assertEqual(5, TestHouse.objects.count())
+        self.assertEqual('', house1.country)
+        self.assertEqual('Australia', house2.country)
+        self.assertEqual('Belgium', house3.country)
+        self.assertEqual('Australia', house4.country)
+        house5 = TestHouse.objects.get(address='House5')
+        self.assertEqual('Australia', house5.country)
+
+    def test_delete(self):
+        house1 = TestHouse.objects.create(address='House1')
+        house2 = TestHouse.objects.create(address='House2')
+
+        csv_file_obj = tempfile.NamedTemporaryFile(mode='w', prefix='TestSystem_tests_TestHouse_', suffix='.csv')
+        csv_file_obj.writelines([
+            'action_flags,match_field_name,address,country\n',
+            'd,address,House1,Australia\n', # Should have no effect
+            'd*,address,House2,Australia\n', # Should delete
+            ])
+        csv_file_obj.seek(0)
+
+        call_command('syncfiles', csv_file_obj.name)
+
+        house1.refresh_from_db()
+        self.assertEqual('', house1.country)
+
+        with self.assertRaises(Exception):
+            house2.refresh_from_db()
+
+    def test_create_and_update_with_external_refs(self):
+        house1 = TestHouse.objects.create(address='House1')
+        house2 = TestHouse.objects.create(address='House2')
+
+        external_system = ExternalSystem.objects.create(name='TestSystem')
+
+        house2mapping = ExternalKeyMapping.objects.create(
+                content_type=ContentType.objects.get_for_model(TestHouse),
+                external_system=external_system,
+                external_key='House2Key',
+                object_id=0)
+
+
+        csv_file_obj = tempfile.NamedTemporaryFile(mode='w', prefix='TestSystem_tests_TestHouse_', suffix='.csv')
+        csv_file_obj.writelines([
+            'external_key,action_flags,match_field_name,address\n',
+            'House1Key,c,address,House1\n', # Should create a key mapping
+            'House2Key,u,address,House2\n', # Should update existing mapping
+            'House3Key,c,address,House3\n', # Should create new house and mapping
+            ])
+        csv_file_obj.seek(0)
+
+        
+        call_command('syncfiles', csv_file_obj.name)
+
+        for object in [house1, house2, house2mapping]:
+            object.refresh_from_db()
+
+        self.assertEqual(3, ExternalKeyMapping.objects.count())
+        self.assertEqual(house2mapping.object_id, house2.id)
+
+    def test_delete_with_external_refs(self):
+        house1 = TestHouse.objects.create(address='House1')
+        house2 = TestHouse.objects.create(address='House2')
+        house3 = TestHouse.objects.create(address='House3')
+        house4 = TestHouse.objects.create(address='House4')
+
+        external_system = ExternalSystem.objects.create(name='TestSystem', label='TestSystem')
+        different_external_system = ExternalSystem.objects.create(name='DifferentSystem', label='DifferentSystem')
+
+        house2mapping = ExternalKeyMapping.objects.create(
+                content_type=ContentType.objects.get_for_model(TestHouse),
+                external_system=external_system,
+                external_key='House2Key',
+                object_id=house2.id)
+
+        house3mapping = ExternalKeyMapping.objects.create(
+                content_type=ContentType.objects.get_for_model(TestHouse),
+                external_system=different_external_system,
+                external_key='House3Key',
+                object_id=house3.id)
+
+        house4mapping = ExternalKeyMapping.objects.create(
+                content_type=ContentType.objects.get_for_model(TestHouse),
+                external_system=different_external_system,
+                external_key='House4Key',
+                object_id=house4.id)
+
+        csv_file_obj = tempfile.NamedTemporaryFile(mode='w', prefix='TestSystem_tests_TestHouse_', suffix='.csv')
+        csv_file_obj.writelines([
+            'external_key,action_flags,match_field_name,address\n',
+            'House1Key,d,address,House1\n', # Should do nothing, as this does not have the final mapping
+            'House2Key,d,address,House2\n', # Should delete, as this IS the final mapping
+            'House3Key,d,address,House3\n', # Should do nothing, as there is another mapping
+            'House4Key,d*,address,House4\n', # Should delete object but leave mapping, as it is forced
+            ])
+        csv_file_obj.seek(0)
+        
+        call_command('syncfiles', csv_file_obj.name)
+
+        self.assertTrue(TestHouse.objects.filter(address='House1').exists())
+        self.assertFalse(TestHouse.objects.filter(address='House2').exists())
+        self.assertTrue(TestHouse.objects.filter(address='House3').exists())
+        self.assertFalse(TestHouse.objects.filter(address='House4').exists())
+        
+        self.assertFalse(ExternalKeyMapping.objects.filter(external_key='House1Key').exists())
+        self.assertFalse(ExternalKeyMapping.objects.filter(external_key='House2Key').exists())
+        self.assertTrue(ExternalKeyMapping.objects.filter(external_key='House3Key').exists())
+        self.assertTrue(ExternalKeyMapping.objects.filter(external_key='House4Key').exists())
+
+    def test_it_does_all_creates_before_deletes(self):
+
+        file1 = tempfile.NamedTemporaryFile(mode='w', prefix='TestSystem1_tests_TestHouse_', suffix='.csv')
+        file1.writelines([
+            'action_flags,match_field_name,address,country\n',
+            'd*,address,House1,Australia\n', # Should delete
+            ])
+        file1.seek(0)
+
+        file2 = tempfile.NamedTemporaryFile(mode='w', prefix='TestSystem2_tests_TestHouse_', suffix='.csv')
+        file2.writelines([
+            'action_flags,match_field_name,address,country\n',
+            'c,address,House1,Australia\n', # Should attempt to create, but should be undone by delete above
+            ])
+        file2.seek(0)
+        call_command('syncfiles', file1.name, file2.name)
+
+        self.assertEqual(0, TestHouse.objects.count())
+
+
+
