0e03eacbc92164674bc80171c67bd9fe1fba2812
Andrew Dodd <andrew.john.dodd@gmail.com>
2015-11-30 18:23:41 +0100

    Including tests app in runtests.py
    I'm pretty sure this is a hack, but hopefully it might see the CI pass.

--- runtests.py
+++ runtests.py
@@ -18,6 +18,7 @@ try:
             "django.contrib.contenttypes",
             "django.contrib.sites",
             "nsync",
+            'tests',
         ],
         SITE_ID=1,
         MIDDLEWARE_CLASSES=(),
