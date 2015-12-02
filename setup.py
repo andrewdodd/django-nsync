aa26bf9f5a70d5b9bc9662c59dad9b5e284f5764
Andrew Dodd <andrew.john.dodd@gmail.com>
2015-12-02 15:21:53 +0100

    Adapting setup.py etc to be a bit more 'best practice' (I hope)
    Once again, heavily borrowing from Hynek:
    https://hynek.me/articles/sharing-your-labor-of-love-pypi-quick-and-dirty/

--- setup.py
+++ setup.py
@@ -3,15 +3,64 @@
 
 import os
 import sys
+import re
 
-import nsync
 
-try:
-    from setuptools import setup
-except ImportError:
-    from distutils.core import setup
+# Start of section from Hynek 
+# https://github.com/hynek/attrs/blob/master/setup.py
+import codecs
+from setuptools import setup, find_packages
 
-version = nsync.__version__
+###############################################################################
+
+PACKAGES = find_packages(where="src")
+META_PATH = os.path.join("src", "nsync", "__init__.py")
+KEYWORDS = []
+CLASSIFIERS = [
+    'Development Status :: 3 - Alpha',
+    'Framework :: Django',
+    'Framework :: Django :: 1.7',
+    'Framework :: Django :: 1.8',
+    'Intended Audience :: Developers',
+    'License :: OSI Approved :: MIT License',
+    'Natural Language :: English',
+    'Programming Language :: Python :: 3',
+    'Programming Language :: Python :: 3.3',
+    'Programming Language :: Python :: 3.4',
+    'Programming Language :: Python :: 3.5',        
+]
+INSTALL_REQUIRES = []
+
+###############################################################################
+
+HERE = os.path.abspath(os.path.dirname(__file__))
+
+
+def read(*parts):
+    """
+    Build an absolute path from *parts* and and return the contents of the
+    resulting file.  Assume UTF-8 encoding.
+    """
+    with codecs.open(os.path.join(HERE, *parts), "rb", "utf-8") as f:
+        return f.read()
+
+
+META_FILE = read(META_PATH)
+
+
+def find_meta(meta):
+    """
+    Extract __*meta*__ from META_FILE.
+    """
+    meta_match = re.search(
+        r"^__{meta}__ = ['\"]([^'\"]*)['\"]".format(meta=meta),
+        META_FILE, re.M
+    )
+    if meta_match:
+        return meta_match.group(1)
+    raise RuntimeError("Unable to find __{meta}__ string.".format(meta=meta))
+
+# End of section from Hynek
 
 if sys.argv[-1] == 'publish':
     try:
@@ -25,6 +74,7 @@ if sys.argv[-1] == 'publish':
 
 if sys.argv[-1] == 'tag':
     print("Tagging the version on github:")
+    version = find_meta('version')
     os.system("git tag -a %s -m 'version %s'" % (version, version))
     os.system("git push --tags")
     sys.exit()
@@ -33,35 +83,21 @@ readme = open('README.rst').read()
 history = open('HISTORY.rst').read().replace('.. :changelog:', '')
 
 setup(
-    name='django-nsync',
-    version=version,
-    description="""It N syncs""",
-    long_description=readme + '\n\n' + history,
-    author='Andrew Dodd',
-    author_email='andrew.john.dodd@gmail.com',
-    url='https://github.com/andrewdodd/django-nsync',
-    packages=[
-        'nsync',
-    ],
-    include_package_data=True,
-    install_requires=[
-    ],
-    license="BSD",
+    name=find_meta("title"),
+    description=find_meta("description"),
+    license=find_meta("license"),
+    url=find_meta("uri"),
+    version=find_meta("version"),
+    author=find_meta("author"),
+    author_email=find_meta("email"),
+    maintainer=find_meta("author"),
+    maintainer_email=find_meta("email"),
+    keywords=KEYWORDS,
+    long_description=read("README.rst"),
+    packages=PACKAGES,
+    package_dir={"": "src"},
     zip_safe=False,
-    keywords='django-nsync',
-    classifiers=[
-        'Development Status :: 3 - Alpha',
-        'Framework :: Django',
-        'Framework :: Django :: 1.7',
-        'Framework :: Django :: 1.8',
-        'Intended Audience :: Developers',
-        'License :: OSI Approved :: BSD License',
-        'Natural Language :: English',
-        'Programming Language :: Python :: 2',
-        'Programming Language :: Python :: 2.7',
-        'Programming Language :: Python :: 3',
-        'Programming Language :: Python :: 3.3',
-        'Programming Language :: Python :: 3.4',
-        'Programming Language :: Python :: 3.5',        
-    ],
+    classifiers=CLASSIFIERS,
+    install_requires=INSTALL_REQUIRES,
 )
+
---
+++ src/nsync/__init__.py
@@ -0,0 +1,15 @@
+
+__version__ = '0.1.0'
+
+__title__ = 'django-nsync'
+__description__ = 'Django N Sync provides a simple way to keep your Django ' \
+                  'Model data N Sync with N external systems'
+__uri__ = 'https://django-nsync.readthedocs.org/'
+
+__author__ = 'Andrew Dodd'
+__email__ = 'andrew.john.dodd@gmail.com'
+
+__license__ = 'MIT'
+__copyright__ = 'Copyright (c) 2015 Andrew Dodd'
+
+# No imports yet, not sure what exactly to import
