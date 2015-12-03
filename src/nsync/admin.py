5dadc80493e89c67ca8b190bcc074caaf3ce421c
Andrew Dodd <andrew.john.dodd@gmail.com>
2015-12-03 15:51:49 +0100

    Tidying up
    Aligning admin with model changes and removing the admin class
    registrations (they should be available to but not forced on the users
    of the library).
    
    Renaming files etc.

--- src/nsync/admin.py
+++ src/nsync/admin.py
@@ -4,15 +4,11 @@ from django.contrib import admin
 from .models import ExternalSystem
 from .models import ExternalKeyMapping
 
-import logging
 
-logger = logging.getLogger(__name__)
-
-@admin.register(ExternalSystem)
 class ExternalSystemAdmin(admin.ModelAdmin):
-    list_display = ('__str__', 'label')
+    list_display = ('name', 'description')
 
-@admin.register(ExternalKeyMapping)
+
 class ExternalKeyMappingAdmin(admin.ModelAdmin):
     fieldsets = (
             ('Internal Object', {
@@ -22,7 +18,4 @@ class ExternalKeyMappingAdmin(admin.ModelAdmin):
                 'fields': ('external_system', 'external_key')
             }),
         ) 
-
-#admin.site.register(ExternalKeyMapping)
-#admin.site.register(ExternalSystem)
 
