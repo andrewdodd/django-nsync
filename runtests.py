928c778a7fe1db81307cee2d7d3c55d24e0b4bdd
Andrew Dodd <andrew.john.dodd@gmail.com>
2015-12-07 16:37:55 +0100

    Fixing local test runner

--- runtests.py
+++ runtests.py
@@ -1,40 +1,39 @@
 import sys
 
-try:
-    from django.conf import settings
-    from django.test.utils import get_runner
+import django
+from django.conf import settings
+from django.test.utils import get_runner
 
-    settings.configure(
-        DATABASES={
-            'default': {
-                'ENGINE': 'django.db.backends.sqlite3',
-            }
-        },
-        INSTALLED_APPS=[
-            'django.contrib.contenttypes',
-            'nsync',
-            'tests',
-        ],
-    )
-
+def setup_env():
+    sys.path.append('./src/')
     try:
-        import django
-        setup = django.setup
-    except AttributeError:
-        pass
-    else:
-        setup()
 
-except ImportError:
-    import traceback
-    traceback.print_exc()
-    raise ImportError('To fix this error, sort out the imports')
+        settings.configure(
+            DATABASES={
+                'default': {
+                    'ENGINE': 'django.db.backends.sqlite3',
+                }
+            },
+            INSTALLED_APPS=[
+                'django.contrib.contenttypes',
+                'nsync',
+                'tests',
+            ],
+        )
+
+        setup = django.setup()
+
+    except ImportError:
+        import traceback
+        traceback.print_exc()
+        raise ImportError('To fix this error, sort out the imports')
 
 
 def run_tests(*test_args):
     if not test_args:
         test_args = ['tests']
 
+    setup_env()
     # Run tests
     TestRunner = get_runner(settings)
     test_runner = TestRunner()
