2236f00e634d3118c6c55912d3daad9c617003e6
Rolandas Imbrasas <rimbrasas@apcglobal.com>
2018-03-06 14:25:39 +1100

    Add Django 2.0 to setup.py Django version classifiers

--- setup.py
+++ setup.py
@@ -20,6 +20,7 @@ CLASSIFIERS = [
     'Framework :: Django',
     'Framework :: Django :: 1.7',
     'Framework :: Django :: 1.8',
+    'Framework :: Django :: 2.0',
     'Intended Audience :: Developers',
     'License :: OSI Approved :: MIT License',
     'Natural Language :: English',
